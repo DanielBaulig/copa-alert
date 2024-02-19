294e4f6dd1de937a89ec93d422978ef98cb943ae
Daniel Baulig <me@danielbaulig.de>
2024-02-18 19:43:33 -0800

    Add ability to stack rank preferred teams for slots

--- ca.py
+++ ca.py
@@ -5,6 +5,24 @@ import requests
 
 path = pathlib.Path(__file__).parent.resolve()
 
+def str_to_time(v):
+    return time(*map(int, v.split(":")))
+
+def fits_in_time_range(event_start, event_end, range_start, range_end):
+    if event_start < range_start:
+        return False
+    if event_end > range_end:
+        return False
+    return True
+
+def fits_in_slot(event_start, event_end, slot):
+    return fits_in_time_range(
+        event_start, 
+        event_end, 
+        str_to_time(slot["start_time"]), 
+        str_to_time(slot["end_time"]),
+    )
+
 with open(f'{path}/.data.json') as f:
     bearer = json.load(f)['bearer']
 
@@ -12,6 +30,7 @@ with open(f'{path}/settings.json') as f:
     settings = json.load(f)
 
 teams = list(settings["teams"])
+id_to_team = { v: k for k, v in settings["teams"].items() }
 
 def get_headers(bearer):
     return {
@@ -59,9 +78,28 @@ if response.status_code not in range(200, 299):
         headers=get_headers(bearer)
     )
 
-customers = [x for x in response.json()["data"] if x["type"] == "customer" and x["id"] in customer_ids]
+response_json = response.json()
+customers = [x for x in response_json["data"] if x["type"] == "customer" and x["id"] in customer_ids]
+customer_events = {x["id"]: x for x in response_json["included"] if x["type"] == "customer-events"}
 
-registered_events = [event["id"].split("-")[1] for customer in customers for event in customer["relationships"]["allEvents"]["data"]]
+registered_customer_event_ids = [event["id"] for customer in customers for event in customer["relationships"]["allEvents"]["data"]]
+registered_event_ids = [customer_event_id.split("-")[1] for customer_event_id in registered_customer_event_ids]
+
+for slot in slots:
+    slot["registered_customer_events"] = []
+    for ce_id in registered_customer_event_ids:
+        customer_event = customer_events[ce_id]["attributes"]
+        start_datetime = datetime.fromisoformat(customer_event["start"])
+        start_time = start_datetime.time()
+        end_time = datetime.fromisoformat(customer_event["end"]).time()
+        team_id = int(customer_event["hteam_id"])
+        if not team_id in id_to_team:
+            continue
+        if not fits_in_slot(start_time, end_time, slot):
+            continue
+        if not id_to_team[team_id] in slot["teams"]:
+            continue
+        slot["registered_customer_events"].append(customer_event)
 
 for team in teams:
     team_id = settings["teams"][team]
@@ -70,33 +108,53 @@ for team in teams:
             headers=get_headers(bearer)
     )
 
-    resp = response.json()
+    try:
+        resp = response.json()
+    except e:
+        print(f'Error {team} {response}')
+        continue
+
     event_summaries = [
         event for event in resp['included'] if event['type'] == 'event-summary'
     ]
 
     for event in event_summaries:
         attributes = event['attributes']
-        if event["id"] in registered_events:
+        if event["id"] in registered_event_ids:
             continue
         if not attributes['registration_status'] == 'open':
             continue
         start_datetime = datetime.fromisoformat(attributes['start_date'])
+        start_date = start_datetime.date()
         start_time = start_datetime.time()
         end_time = datetime.fromisoformat(attributes['end_date']).time()
         weekday = start_datetime.weekday()
 
         for slot in slots:
-            if team not in slot["teams"]:
+            if not team in slot["teams"]:
                 continue
-            if "date" in slot and not start_datetime.date() == datetime.fromisoformat(slot["date"]).date():
+            if "date" in slot and not start_date == datetime.fromisoformat(slot["date"]).date():
                 continue
             if "day_of_week" in slot and not start_datetime.weekday() == slot["day_of_week"]:
                 continue
-            if start_time < time(*map(int, slot["start_time"].split(":"))):
-                continue
-            if end_time > time(*map(int,slot["end_time"].split(":"))):
+            if not fits_in_slot(start_time, end_time, slot):
                 continue
 
+            remaining_teams = slot["teams"].copy()
+            # Check if any of the other registered events for this slot
+            # is higher priority
+            for registered_customer_event in slot["registered_customer_events"]:
+                if start_date != datetime.fromisoformat(registered_customer_event["start"]).date():
+                    continue
+
+                registered_team = id_to_team[int(registered_customer_event["hteam_id"])]
+                if not registered_team in remaining_teams:
+                    continue
+                idx = remaining_teams.index(registered_team)
+                remaining_teams = remaining_teams[:idx]
+
+            if not team in remaining_teams:
+                continue
+            
             print(f'{attributes["name"]}, {start_datetime.strftime("%A %m/%d")} {start_time.strftime("%H:%M")}-{end_time.strftime("%H:%M")}, {attributes["open_slots"]} slots')
             break
